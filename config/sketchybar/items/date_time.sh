cb88759027c0b62fef9211d395adaae0f305687b
Charles Vincent <cjmvincent@outlook.com>
2024-10-06 16:45:56 -0400

    more skecthybar plugins

--- config/sketchybar/items/date_time.sh
+++ config/sketchybar/items/date_time.sh
@@ -8,9 +8,11 @@ sketchybar -m --add       item               time             right             
               --set       date               update_freq=60                                                \
                                              background.color=0xffe8e8e9                                   \
                                              label.color=0xff000000                                        \
-                                             label.font="SF Pro:Semibold:12"                               \
+                                             label.font="SpaceMono Nerd Font:Semibold:13"                  \
                                              icon.padding_right=0                                          \
-                                             label.padding_left=0                                          \
-                                             background.height=15                                          \
-                                             background.corner_radius=4                                    \
+                                             label.padding_left=3                                          \
+					     label.padding_right=3					   \
+					     padding_left=10						   \
+                                             background.height=26                                          \
+                                             background.corner_radius=8                                    \
                                              script="~/.config/sketchybar/plugins/date.sh"
--- config/sketchybar/items/weather.sh
+++ config/sketchybar/items/weather.sh
@@ -3,7 +3,7 @@
 weather=(
     updates=on
     update_freq=600 
-    script="$PLUGIN_DIR/weather.sh" 
+    script="$PLUGIN_DIR/weather.lua" 
     icon.font="$FONT:Regular:13.0" 
     background.drawing=on 
 	padding_right=5 
