9d4daddd63d96f9d8bfb59f5f72a1bff3800b0bc
Charles Vincent <cjmvincent@outlook.com>
2025-03-17 07:48:52 -0400

    include login script to launch everyday apps

---
+++ config/skhd/login.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+open -na "/Applications/Google Chrome.app"
+open -na "/Applications/Visual Studio Code.app"
+open -na "/Applications/Microsoft Outlook.app"
+open -na "/Applications/Microsoft Teams.app"
