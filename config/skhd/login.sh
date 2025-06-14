671095fe1aabb1cd5ad7c5e55f86975dd8da0a08
Charles Vincent <cjmvincent@outlook.com>
2025-06-14 18:53:28 -0400

    spaces Spaces SPACES

--- config/skhd/login.sh
+++ config/skhd/login.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 open -na "/Applications/Google Chrome.app"
+sleep 1
 open -na "/Applications/Visual Studio Code.app"
+sleep 1
 open -na "/Applications/Microsoft Outlook.app"
+sleep 1
 open -na "/Applications/Microsoft Teams.app"
