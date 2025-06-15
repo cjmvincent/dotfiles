ecf3b9d56415d9966868ad8b7de5f085f6d4e927
Charles Vincent <cjmvincent@outlook.com>
2025-06-15 19:53:55 -0400

    add a couple of apps to launch script

--- config/skhd/login.sh
+++ config/skhd/login.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-open -na "/Applications/Google Chrome.app"
-sleep 1
-open -na "/Applications/Visual Studio Code.app"
-sleep 1
+
 open -na "/Applications/Microsoft Outlook.app"
-sleep 1
+sleep 5
 open -na "/Applications/Microsoft Teams.app"
+sleep 5
+open -na "/Applications/Google Chrome.app"
+sleep 5
+open -na "/Applications/Visual Studio Code.app"
+sleep 5
+open -na "/Applications/Microsoft OneNote.app"
